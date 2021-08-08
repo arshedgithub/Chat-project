c8d87ddeee8061b0e53b341eb3c9820789fd8778
arshedgithub <arshedahmedmail@gmail.com>
2021-08-08 18:18:55 +0530

    initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import 'firebase/auth'
+import 'firebase/analytics'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAGe7iMyy8i8raxEgn4zP9RHv6xxTGd_yo",
+  authDomain: "chat-project-7b45d.firebaseapp.com",
+  projectId: "chat-project-7b45d",
+  storageBucket: "chat-project-7b45d.appspot.com",
+  messagingSenderId: "543473326045",
+  appId: "1:543473326045:web:8e9808af06bc6f6acf4834",
+  measurementId: "G-0N1SZEC11C"
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>chat project</h1>
   );
 }
 
